4a3b06ac070f7f35574e3b835b3848c06bd6f61d
yedf2 <120050102@qq.com>
2021-12-02 15:51:12 +0800

    add ParseServerMethod

--- driver.go
+++ driver.go
@@ -4,9 +4,14 @@ import "fmt"
 
 // Driver interface to do service register and discover
 type Driver interface {
+	// GetName return the name of the driver
 	GetName() string
+	// RegisterGrpcResolver register the grpc resolver to handle custom scheme
 	RegisterGrpcResolver()
-	RegisterService(url string, endpoint string) error
+	// RegisterGrpcService register dtm endpoint to target
+	RegisterGrpcService(target string, endpoint string) error
+	// ParseServerMethod parse the url to server and method. server is used to construct grpc connection, method is used to call particular method
+	ParseServerMethod(url string) (svr string, method string, err error)
 }
 
 var (
