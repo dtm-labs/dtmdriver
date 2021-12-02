86dce681d5db6297d64e30ca13bb29e148049d4b
yedf2 <120050102@qq.com>
2021-12-02 17:23:30 +0800

    change usage

--- driver.go
+++ driver.go
@@ -1,7 +1,5 @@
 package dtmdriver
 
-import "fmt"
-
 // Driver interface to do service register and discover
 type Driver interface {
 	// GetName return the name of the driver
@@ -13,21 +11,4 @@ type Driver interface {
 	// ParseServerMethod parse the uri to server and method.
 	// server will be passed to grpc.Dial, and method to grpc.ClientConn.invoke
 	ParseServerMethod(uri string) (server string, method string, err error)
-}
-
-var (
-	drivers = map[string]Driver{}
-)
-
-// Register used by each driver writer
-func Register(driver Driver) {
-	drivers[driver.GetName()] = driver
-}
-
-func MustGetDriver(name string) Driver {
-	v := drivers[name]
-	if v == nil {
-		panic(fmt.Errorf("no dtm driver with name: %s has been registered", name))
-	}
-	return v
 }
